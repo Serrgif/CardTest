639d459e235af6d4ea8e8063812523e9adea05bf
Irina <sergei32git@gmail.com>
2024-10-18 11:52:51 +0500

    Правки в тестах

--- src/test/java/SendFormTest.java
+++ src/test/java/SendFormTest.java
@@ -51,7 +51,7 @@ public class SendFormTest {
         form.$("[data-test-id='phone'] input").setValue("+79270000000");
         form.$("[data-test-id='agreement']").click();
         form.$("button").click();
-        $("[data-test-id='name'].inputinvalid.input__sub")
+        $("[data-test-id='name'].input__sub")
                 .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
@@ -63,8 +63,32 @@ public class SendFormTest {
         form.$("[data-test-id='phone'] input").setValue("+792700000");
         form.$("[data-test-id='agreement']").click();
         form.$("button").click();
-        $("[data-test-id='phone'].inputinvalid.input__sub")
+        $("[data-test-id='phone'].input__sub")
                 .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void sendFormTestV6() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("");
+        form.$("[data-test-id='phone'] input").setValue("+792700000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='name'].input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void sendFormTestV7() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Сергей Новиков");
+        form.$("[data-test-id='phone'] input").setValue("");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='phone'].input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
 }

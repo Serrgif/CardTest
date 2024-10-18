3d2c4cefbf1e4a02f4db2950b8e0445e586deff2
Irina <sergei32git@gmail.com>
2024-10-18 11:45:05 +0500

    Добавление тестов на невалидные данные

--- src/test/java/SendFormTest.java
+++ src/test/java/SendFormTest.java
@@ -19,6 +19,54 @@ public class SendFormTest {
                 .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    void sendFormTestV2() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Сергей");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']")
+                .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void sendFormTestV3() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Сергей Новиков-Новиков");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']")
+                .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void sendFormTestV4() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Sergei");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='name'].inputinvalid.input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void sendFormTestV5() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Сергей Новиков");
+        form.$("[data-test-id='phone'] input").setValue("+792700000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='phone'].inputinvalid.input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
 }
 
 

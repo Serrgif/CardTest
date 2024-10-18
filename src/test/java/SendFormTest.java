969d1607954941d9f18fd0f0bfeded29cb80a2d1
Irina <sergei32git@gmail.com>
2024-10-18 11:57:23 +0500

    Первый тест на невалидное Имя

--- src/test/java/SendFormTest.java
+++ src/test/java/SendFormTest.java
@@ -43,7 +43,18 @@ public class SendFormTest {
                 .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-    
+    @Test
+    void sendFormTestV4() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='name'] input").setValue("Sergei");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='name'].input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
 
 }
 

478d863c859caa2ecd65afd3019826aee725706d
Irina <sergei32git@gmail.com>
2024-10-18 11:55:40 +0500

    Удаление тестов на невалидные данные

--- src/test/java/SendFormTest.java
+++ src/test/java/SendFormTest.java
@@ -43,53 +43,7 @@ public class SendFormTest {
                 .shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-    @Test
-    void sendFormTestV4() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id='name'] input").setValue("Sergei");
-        form.$("[data-test-id='phone'] input").setValue("+79270000000");
-        form.$("[data-test-id='agreement']").click();
-        form.$("button").click();
-        $("[data-test-id='name'].input__sub")
-                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void sendFormTestV5() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id='name'] input").setValue("Сергей Новиков");
-        form.$("[data-test-id='phone'] input").setValue("+792700000");
-        form.$("[data-test-id='agreement']").click();
-        form.$("button").click();
-        $("[data-test-id='phone'].input__sub")
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void sendFormTestV6() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id='name'] input").setValue("");
-        form.$("[data-test-id='phone'] input").setValue("+792700000");
-        form.$("[data-test-id='agreement']").click();
-        form.$("button").click();
-        $("[data-test-id='name'].input__sub")
-                .shouldHave(exactText("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void sendFormTestV7() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id='name'] input").setValue("Сергей Новиков");
-        form.$("[data-test-id='phone'] input").setValue("");
-        form.$("[data-test-id='agreement']").click();
-        form.$("button").click();
-        $("[data-test-id='phone'].input__sub")
-                .shouldHave(exactText("Поле обязательно для заполнения"));
-    }
+    
 
 }
 
